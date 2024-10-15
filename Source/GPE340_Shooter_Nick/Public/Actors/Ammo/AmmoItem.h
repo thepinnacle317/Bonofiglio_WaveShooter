1a21c9ab271ee009d3ee07acb75085ee5099ef19
Nick Bonofiglio <nick.bono0317@gmail.com>
2024-10-15 08:58:29 -0700

    Ammo Pickups

---
+++ Source/GPE340_Shooter_Nick/Public/Actors/Ammo/AmmoItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Item_Base.h"
+#include "AmmoItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPE340_SHOOTER_NICK_API AAmmoItem : public AItem_Base
+{
+	GENERATED_BODY()
+	
+};
